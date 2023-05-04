2ef830d64cd4dabac53c1354b1989a47ef90deec
José Junior <77794636+kwaark@users.noreply.github.com>
2023-05-04 14:12:28 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,12 +5,12 @@ describe('App', () => {
   it('should render login section by default', () => {
     render(<App />);
     const loginSection = screen.getAllByText(/Login/i);
-    expect(loginSection).toBeInTheDocument();
+    expect(loginSection).toHaveLength(1);
   });
 
   it('should switch to register when register link is clicked', () => {
     render(<App />);
-    const registerLink = screen.getAllByText(/Cadastre-se/i);
+    const registerLink = screen.getAllByText(/Cadastre-se/i)[0];
     fireEvent.click(registerLink);
     const registerSection = screen.getByText(/Cadastro/i);
     expect(registerSection).toBeInTheDocument();
