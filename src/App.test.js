ffecf0a6c283cb87f1e03ffdb4018305093bac5f
José Junior <77794636+kwaark@users.noreply.github.com>
2023-05-04 13:45:41 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  test('renders login section by default', () => {
+  it('should render login section by default', () => {
     render(<App />);
-    const loginSection = screen.getByText(/login/i);
+    const loginSection = screen.getByText(/Login/i);
     expect(loginSection).toBeInTheDocument();
   });
 
-  test('shows registration form when link is clicked', () => {
+  it('should switch to register when register link is clicked', () => {
     render(<App />);
-    const signUpLink = screen.getByText(/cadastre-se/i);
-    signUpLink.click();
-
-    const registrationForm = screen.getByText(/cadastro/i);
-    expect(registrationForm).toBeInTheDocument();
+    const registerLink = screen.getByText(/Cadastre-se/i);
+    fireEvent.click(registerLink);
+    const registerSection = screen.getByText(/Cadastro/i);
+    expect(registerSection).toBeInTheDocument();
   });
 });
