32a9dea6ccedd485a98a2e0fcf2c06866a76bc67
José Junior <77794636+kwaark@users.noreply.github.com>
2023-05-04 15:37:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,73 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="container" >
-    <a class="links" id="paracadastro" href="#paracadastro"></a>
-    <a class="links" id="paralogin" href="#paralogin"></a>
-    
-    <div className="content">      
-      <div id="login">
-        <form method="post" action=""> 
-          <h1>Login</h1> 
-          <p> 
-            <label htmlFor="email_login">Seu e-mail</label>
-            <input id="email_login" name="email_login" required="required" type="text" placeholder="dev@ops.com"/>
-          </p>
-          
-          <p> 
-            <label htmlFor="senha_login">Sua senha</label>
-            <input id="senha_login" name="senha_login" required="required" type="password" placeholder="1234" /> 
-          </p>
-          
-          <p> 
-            <input type="checkbox" name="manterlogado" id="manterlogado" value="" /> 
-            <label htmlFor="manterlogado">Manter-me logado</label>
-          </p>
-          
-          <p> 
-            <input type="submit" value="Logar" /> 
-          </p>
-          
-          <p className="link">
-            Ainda não tem conta?
-            <a href="#paracadastro">Cadastre-se</a>
-          </p>
-        </form>
-      </div>
+    <div className="container">
+      <a className="links" id="paracadastro" href="#paracadastro">Cadastrar</a>
+      <a className="links" id="paralogin" href="#paralogin">Login</a>
+
+      <div className="content">
+        <div id="login">
+          <form method="post" action="">
+            <h1>Login</h1>
+            <p>
+              <label htmlFor="email_login">Seu e-mail</label>
+              <input id="email_login" name="email_login" required="required" type="text" placeholder="dev@ops.com"/>
+            </p>
+
+            <p>
+              <label htmlFor="senha_login">Sua senha</label>
+              <input id="senha_login" name="senha_login" required="required" type="password" placeholder="1234"/>
+            </p>
+
+            <p>
+              <input type="checkbox" name="manterlogado" id="manterlogado" value=""/>
+              <label htmlFor="manterlogado">Manter-me logado</label>
+            </p>
+
+            <p>
+              <input type="submit" value="Logar"/>
+            </p>
+
+            <p className="link">
+              Ainda não tem conta?
+              <a href="#paracadastro">Cadastre-se</a>
+            </p>
+          </form>
+        </div>
 
         <div id="cadastro">
-        <form method="post" action=""> 
-          <h1>Cadastro</h1> 
-          
-          <p> 
-            <label htmlFor="nome_cad">Seu nome</label>
-            <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="Luiz Kelvin" />
-          </p>
-          
-          <p> 
-            <label htmlFor="email_cad">Seu e-mail</label>
-            <input id="email_cad" name="email_cad" required="required" type="email" placeholder="dev@ops.com"/> 
-          </p>
-          
-          <p> 
-            <label htmlFor="senha_cad">Sua senha</label>
-            <input id="senha_cad" name="senha_cad" required="required" type="password" placeholder="1234"/>
-          </p>
-          
-          <p> 
-            <input type="submit" value="Cadastrar"/> 
-          </p>
-          
-          <p className="link">  
-            Já tem conta?
-            <a href="#paralogin"> Ir para Login </a>
-          </p>
-        </form>
+          <form method="post" action="">
+            <h1>Cadastro</h1>
+
+            <p>
+              <label htmlFor="nome_cad">Seu nome</label>
+              <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="Luiz Kelvin"/>
+            </p>
+
+            <p>
+              <label htmlFor="email_cad">Seu e-mail</label>
+              <input id="email_cad" name="email_cad" required="required" type="email" placeholder="dev@ops.com"/>
+            </p>
+
+            <p>
+              <label htmlFor="senha_cad">Sua senha</label>
+              <input id="senha_cad" name="senha_cad" required="required" type="password" placeholder="1234"/>
+            </p>
+
+            <p>
+              <input type="submit" value="Cadastrar"/>
+            </p>
+
+            <p className="link">
+              Já tem conta?
+              <a href="#paralogin"> Ir para Login </a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
-  </div> 
   );
 }
 
